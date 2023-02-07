940ddecea8348256f3a441c1a57940be5d1bccfd
Aditya Prakash <aditya.prakash@jellyfishtechnologies.com>
2023-02-07 16:30:13 +0530

    added app name to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Stack } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import {logo} from "../utils/constants"
 import SearchBar from "./SearchBar";
@@ -10,6 +10,7 @@ const Navbar=()=>{
         <Stack direction="row" alignItems="center" p={2} sx={{position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between'}}>
             <Link to="/" style={{display: 'flex', alignItems: 'center'}}>
                 <img src={logo} alt="ClipZ logo" height={50} />
+                <Typography variant="h6" color="#FFF" fontWeight={'bold'} fontStyle='italic' >ClipZ</Typography>
             </Link>
             <SearchBar />
         </Stack>
